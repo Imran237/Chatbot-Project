630cc994f4f6bfadfc379c3e1226837cc1212f46
Imrane Jinadu <imranjinadu57@gmail.com>
2025-09-16 21:58:52 +0000

    Prepare for deployment: created public folder, added render.yaml, updated config.php

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "medical_chatbot_db";
+// Use environment variables for production
+$servername = getenv('DB_HOST') ?: "localhost";
+$username   = getenv('DB_USER') ?: "root";
+$password   = getenv('DB_PASS') ?: "";
+$dbname     = getenv('DB_NAME') ?: "medical_chatbot_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,7 +18,7 @@ $HUGGINGFACE_API_KEY = getenv('HUGGINGFACE_API_KEY');
 // Hugging Face API config
 $HUGGINGFACE_MODELS = [
     "gpt2",
-    "distilgpt2",
+    "distilgpt2", 
     "mrm8488/t5-base-finetuned-common_gen",
     "microsoft/DialoGPT-small"
 ];
@@ -26,5 +27,4 @@ $HUGGINGFACE_MODEL = $HUGGINGFACE_MODELS[0];
 $HUGGINGFACE_API_URL = "https://api-inference.huggingface.co/models/";
 
 define('DEBUG_MODE', true);
-
-// (same get_working_model function…)
+?>
